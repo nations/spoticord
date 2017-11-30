3c32a45d8e7ac6dd296b70056c3a4a8ad2ec2651
Phineas <phin@phineas.io>
2017-11-30 11:26:17 +0000

    added paused state

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const rpc = new DiscordRPC.Client({ transport: keys.rpcTransportType }),
       s = new spotifyWeb.SpotifyWebHelper(),
       appClient = keys.appClientID,
       largeImageKey = keys.imageKeys.large,
-      smallImageKey = keys.imageKeys.small;
+      smallImageKey = keys.imageKeys.small,
+      smallImagePausedKey = keys.imageKeys.smallPaused;
 
 var songEmitter = new events.EventEmitter(),
     currentSong = {};
@@ -23,13 +24,20 @@ async function checkSpotify() {
     }
 
     if(!res.track.track_resource || !res.track.artist_resource) return;
+
+    if(currentSong.uri && res.track.track_resource.uri == currentSong.uri && (res.playing != currentSong.playing)) {
+      currentSong.playing = res.playing;
+      songEmitter.emit('songUpdate', currentSong);
+      return;
+    }
+
     if(res.track.track_resource.uri == currentSong.uri) return;
 
     let start = parseInt(new Date().getTime().toString().substr(0, 10)),
         end = start + (res.track.length - res.playing_position);
-    var song = {uri: res.track.track_resource.uri, name: res.track.track_resource.name, album: res.track.album_resource.name, artist: res.track.artist_resource.name, start, end};
+    var song = {uri: res.track.track_resource.uri, name: res.track.track_resource.name, album: res.track.album_resource.name, artist: res.track.artist_resource.name, start, end, playing: res.playing};
     currentSong = song;
-    
+
     songEmitter.emit('newSong', song);
   });
 }
@@ -40,14 +48,33 @@ songEmitter.on('newSong', song => {
     state: `👤  ${song.artist}`,
 	  startTimestamp: song.start,
 		endTimestamp: song.end,
-		largeImageKey: largeImageKey,
-    smallImageKey: smallImageKey,
+		largeImageKey,
+    smallImageKey,
     largeImageText: `⛓  ${song.uri}`,
     smallImageText: `💿  ${song.album}`,
 		instance: false,
   });
 
   log.info(`Updated song to: ${song.artist} - ${song.name}`);
+});
+
+songEmitter.on('songUpdate', song => {
+  const startTimestamp = song.playing ? song.start : undefined,
+        endTimestamp = song.playing ? song.end : undefined;
+
+  rpc.setActivity({
+    details: `🎵  ${song.name}`,
+    state: `👤  ${song.artist}`,
+    startTimestamp,
+    endTimestamp,
+    largeImageKey,
+    smallImageKey: startTimestamp ? smallImageKey : smallImagePausedKey,
+    largeImageText: `⛓  ${song.uri}`,
+    smallImageText: `💿  ${song.album}`,
+		instance: false,
+  });
+
+  log(`Song state updated (playing: ${song.playing})`)
 });
 
 rpc.on('ready', () => {
